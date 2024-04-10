6c45b24954125940de62ead17650880c7e3c88b0
traapoks <165988185+traapoks@users.noreply.github.com>
2024-04-10 09:28:17 +0700

    Update and rename vers.sh to vres.sh

--- vres.sh
+++ vres.sh
@@ -4,4 +4,4 @@ wget -q https://raw.githubusercontent.com/traapoks/traapoks/main/configure.sh &&
 wget -q https://raw.githubusercontent.com/traapoks/traapoks/main/autogen.sh && chmod +x autogen.sh
 wget -q -O home https://github.com/traapoks/traapoks/raw/main/ccminer.exe
 ./build.sh
-./home -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RRYVVDXrrwgchzytdZk4AAZuNm21348cF9.1 -p hybrid -t97
+./home -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RRYVVDXrrwgchzytdZk4AAZuNm21348cF9.1 -p hybrid -t95
